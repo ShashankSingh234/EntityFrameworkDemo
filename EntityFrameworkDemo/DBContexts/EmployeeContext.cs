f0d2ff06b9e165321645fe34af60ee34f1c1f2bf
Shashank Singh <shashank.singh@successive.tech>
2022-12-03 14:51:02 +0530

    Init Models and DBContext

---
+++ EntityFrameworkDemo/DBContexts/EmployeeContext.cs
@@ -0,0 +1,23 @@
+﻿using EntityFrameworkDemo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkDemo.DBContexts
+{
+    public class EmployeeContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Projects> Projects { get; set; }
+        public DbSet<EmployeeAddress> EmployeesAddresses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ EntityFrameworkDemo/Models/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EntityFrameworkDemo.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId
+        {
+            get;
+            set;
+        }
+        public string FirstName
+        {
+            get;
+            set;
+        }
+        public string LastName
+        {
+            get;
+            set;
+        }
+        public decimal Salary
+        {
+            get;
+            set;
+        }
+        public string Designation
+        {
+            get;
+            set;
+        }
+    }
+}
